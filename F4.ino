ec1e0195bce2aa69d608191d6357985a0fe45053
fer-afk1 <fertorres11188@gmail.com>
2026-05-11 00:04:43 -0600

    Update F4.ino
    a

--- F4.ino
+++ F4.ino
@@ -1,6 +1,3 @@
-// F4 - Calibrar umbral de detección táctil
-// LIS428 - Fundamentos de IoT | Universidad De La Salle Bajío
-
 const int PIN_TACTIL = T0;
 int umbral = 0;
 
@@ -10,14 +7,13 @@ void setup() {
   Serial.println("Calibrando... no toques el pin");
   delay(2000);
 
-  // Toma 10 lecturas sin contacto para calcular el umbral
   long suma = 0;
   for (int i = 0; i < 10; i++) {
     suma += touchRead(PIN_TACTIL);
     delay(100);
   }
   int promedio = suma / 10;
-  umbral = promedio * 0.6; // 60% del valor libre = umbral de toque
+  umbral = promedio * 0.6; 
 
   Serial.print("Promedio sin contacto: ");
   Serial.println(promedio);
